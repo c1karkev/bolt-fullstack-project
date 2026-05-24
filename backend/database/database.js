0cf5b1bfa6e01745a960c49e9039d1965ad06017
008kevin <kardoskevin07@gmail.com>
2026-05-24 23:21:09 +0200

    add price to database plus user ui

--- backend/database/database.js
+++ backend/database/database.js
@@ -23,6 +23,7 @@ function setupProductsTable() {
     let sql = `CREATE TABLE IF NOT EXISTS products (id INT PRIMARY KEY AUTO_INCREMENT,
         name VARCHAR(255),
         description MEDIUMTEXT,
+        price INT,
         imagePath TINYTEXT
     )`;
     con.query(sql, (err, result) => {
